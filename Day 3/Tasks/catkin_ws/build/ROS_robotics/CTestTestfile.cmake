edd3ec08474a3ecf3a712ebab1b0e2af3b9aa6c4
Eslam-Ayman8 <eslamayman13@hotmail.com>
2020-09-28 17:38:00 +0200

    Creating Packages

---
+++ Day 3/Tasks/catkin_ws/build/ROS_robotics/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/eslam/catkin_ws/src/ROS_robotics
+# Build directory: /home/eslam/catkin_ws/build/ROS_robotics
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
