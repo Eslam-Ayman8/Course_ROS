9216eddf6ff8b403b4fedc4c6e0d8e5533b0bf38
Eslam-Ayman8 <eslamayman13@hotmail.com>
2020-09-29 20:02:53 +0200

    add .cpp and .py in the package

---
+++ Day 4/task_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/eslam/ROS_Course/Day 4/task_ws/src
+# Build directory: /home/eslam/ROS_Course/Day 4/task_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("task")
---
+++ Day 4/task_ws/build/task/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/eslam/ROS_Course/Day 4/task_ws/src/task
+# Build directory: /home/eslam/ROS_Course/Day 4/task_ws/build/task
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
