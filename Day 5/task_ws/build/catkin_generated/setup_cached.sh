5a528253ac6446bfdb0f2fa00eba8b3fd1eeb09d
Eslam-Ayman8 <eslamayman13@hotmail.com>
2020-10-08 00:56:31 +0200

    Creating Packages in Day 5

---
+++ Day 5/task_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/eslam/ROS_Course/Day 5/task_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/eslam/ROS_Course/Day 5/task_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/eslam/ROS_Course/Day 5/task_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/eslam/ROS_Course/Day 5/task_ws/src:$ROS_PACKAGE_PATH"
---
+++ Day 5/task_ws/build/light_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/eslam/ROS_Course/Day 5/task_ws/src/light_robot
+# Build directory: /home/eslam/ROS_Course/Day 5/task_ws/build/light_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
