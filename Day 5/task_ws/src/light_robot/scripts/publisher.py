d48f5dccff0960edf19aa3cda9a3e9c858bed505
Eslam-Ayman8 <eslamayman13@hotmail.com>
2020-10-14 03:29:54 +0200

    Publisher.py edited as Counter from 0

--- Day 5/task_ws/src/light_robot/scripts/publisher.py
+++ Day 5/task_ws/src/light_robot/scripts/publisher.py
@@ -2,22 +2,20 @@
 
 import rospy
 from std_msgs.msg import Int32
-if __name__ == '__main__':
 
-    rospy.init_node("node_1", anonymous=False)
-    rate = rospy.Rate(5) # ROS Rate at 5Hz
-    pub = rospy.Publisher("topic_1", Int32, queue_size=10)
-    counter = 0
-
-	
-
-
+def publisher():
+    pub = rospy.Publisher('topic_name', Int32, queue_size=10)
+    rospy.init_node('publisher')
+    r = rospy.Rate(30)
+    
+    count = 0
+    msg = Int32()
     while not rospy.is_shutdown():
-        counter += 1
-        msg = Int32()
-        msg.data = counter
-        pub.publish(counter)
-        rate.sleep()
-		
+        msg.data = count
+        count = count+1 if count >= 0 else 1
+        pub.publish(msg)
+        rospy.loginfo(msg)
+        r.sleep()
 
-
+if __name__ == '__main__':
+    publisher()
