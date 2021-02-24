d55a00b148d56134ccfe5538307f4b49c83472a0
Eslam-Ayman8 <eslamayman13@hotmail.com>
2021-02-24 22:57:23 +0200

    Model + World Created, URDF and Plugins created

---
+++ Day 7/task_ws/gazebo_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/eslam/ROS_Course/Day 7/task_ws/gazebo_ws/src
+# Build directory: /home/eslam/ROS_Course/Day 7/task_ws/gazebo_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ Day 7/task_ws/gazebo_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/eslam/ROS_Course/Day 7/task_ws/gazebo_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
